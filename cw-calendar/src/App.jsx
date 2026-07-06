9f8186c9a0ca18f7f5a0f5752ab5c0f283023734
ksenias <kseniatsn@gmail.com>
2026-07-06 19:26:48 +0300

    календарь_версия_1

--- cw-calendar/src/App.jsx
+++ cw-calendar/src/App.jsx
@@ -4,15 +4,27 @@ import dayGridPlugin from "@fullcalendar/daygrid"
 import interactionPlugin from "@fullcalendar/interaction"
 import { supabase } from "./supabaseClient"
 import localeRu from "@fullcalendar/core/locales/ru";
+import './App.css';
 
 export default function App() {
   const [dancer, setDancer] = useState(null)
   const [events, setEvents] = useState([])
   const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDateWeekend, setSelectedDateWeekend] = useState(null);
 
-  const slots = [
+  const slotsWeek = [
+    "19:00",
+    "19:30",
+    "20:00"
+  ];
+  const slotsWeekend = [
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
     "18:00",
-    "18:30",
     "19:00",
     "19:30",
     "20:00"
@@ -69,9 +81,8 @@ export default function App() {
 
     const events = Object.entries(grouped).map(([time, list]) => ({
       start: time,
-      title: list.join(", ")
+      title: list.join(", ") + ';'
     }))
-    console.log(data);
     setEvents(events)
   }
 
@@ -123,35 +134,18 @@ export default function App() {
         Расписание репетиций CW{" "}
         {dancer ? `(${dancer.name})` : "(неопознанный объект)"}
       </h2>
-
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        events={events}
-        eventTimeFormat={{
-          hour: '2-digit',
-          minute: '2-digit',
-          meridiem: false,
-          hour12: false
-        }}
-        timeZone='Europe/Moscow'
-        dateClick={(info) => {
-          toggleSlot(info.dateStr),
-            setSelectedDate(info.dateStr)
-        }}
-        eventClick={(info) => toggleSlot(info.event.startStr)}
-      />
       {selectedDate && (
         <div
           style={{
             display: "grid",
             gridTemplateColumns: "repeat(4, 100px)",
             gap: 10,
-            marginTop: 20
+            marginTop: 20,
+            marginBottom: 20
           }}
         >
-          {slots.map((time) => (
-            <button
+          {slotsWeek.map((time) => (
+            <button className="button"
               key={time}
               onClick={() => toggleSlot(`${selectedDate}T${time}:00`)}
             >
@@ -160,6 +154,54 @@ export default function App() {
           ))}
         </div>
       )}
+      {selectedDateWeekend && (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(10, 100px)",
+            gap: 10,
+            marginTop: 20,
+            marginBottom: 20
+          }}
+        >
+          {slotsWeekend.map((time) => (
+            <button className="button"
+              key={time}
+              onClick={() => toggleSlot(`${selectedDateWeekend}T${time}:00`)}
+            >
+              {time}
+            </button>
+          ))}
+        </div>
+      )}
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridWeek"
+        firstDay={1}
+        selectable={true}
+        events={events}
+        height={500}
+        eventTimeFormat={{
+          hour: '2-digit',
+          minute: '2-digit',
+          meridiem: false,
+          hour12: false
+        }}
+        timeZone='Europe/Moscow'
+        dateClick={(info) => {
+          const dayOfWeek = info.date.getDay();
+          if (dayOfWeek === 0 || dayOfWeek === 6) {
+            setSelectedDateWeekend(info.dateStr)
+            setSelectedDate(null);
+          } else {
+            setSelectedDate(info.dateStr)
+            setSelectedDateWeekend(null)
+          }
+
+        }}
+        eventClick={(info) => toggleSlot(info.event.startStr)}
+      />
+
     </div>
   )
 }
